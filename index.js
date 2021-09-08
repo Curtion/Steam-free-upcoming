e8e67500e7c9596cb47391cfcd154f4ba7196b48
Curtion <curtion@126.com>
2021-09-08 16:57:23 +0800

    测试消息接收

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client, Intents } = require("discord.js");
+const fs = require("fs");
 const { token } = require("./config.json");
 
 const client = new Client({
@@ -15,7 +16,11 @@ client.once("ready", () => {
   console.log("Ready!");
 });
 client.on("messageCreate", (message) => {
-  console.log(message);
+  fs.appendFile("msg.log", JSON.stringify(message) + "\n", (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
 });
-console.log("Reading");
 client.login(token);
