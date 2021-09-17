74e0f390c0523db0837d58f6632a102798de71f2
Curtion <curtion@126.com>
2021-09-17 11:35:07 +0800

    对接server酱

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, Intents } = require("discord.js");
-const fs = require("fs");
 const { token } = require("./config.json");
+const https = require("https");
+const qs = require("qs");
 
 const client = new Client({
   intents: [
@@ -16,11 +17,42 @@ client.once("ready", () => {
   console.log("Ready!");
 });
 client.on("messageCreate", (message) => {
-  fs.appendFile("msg.log", JSON.stringify(message) + "\n", (err) => {
-    if (err) {
-      console.error(err);
-      return;
+  try {
+    const author = message.authorId;
+    let gameInfo = {};
+    if (author === "884623552584769546") {
+      gameInfo.title = message.embeds.title;
+      gameInfo.url = message.embeds.url;
+      gameInfo.description = message.embeds.description;
     }
-  });
+    const data = qs.stringify({
+      text: gameInfo.title,
+      desp: gameInfo.description,
+    });
+    const req = https.request(
+      {
+        hostname: "sctapi.ftqq.com",
+        port: 443,
+        method: "POST",
+        path: "/SCT76304TUUqaQzaO4LuZPPPnpoxGjOf2.send",
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded",
+          "Content-Length": data.length,
+        },
+      },
+      (res) => {
+        res.on("data", (d) => {
+          console.log(d);
+        });
+      }
+    );
+    req.on("error", (error) => {
+      console.error(error);
+    });
+    req.write(data);
+    req.end();
+  } catch (error) {
+    console.error(error);
+  }
 });
 client.login(token);
