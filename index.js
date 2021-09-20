008914a1dc5ca5f2bfc919ea7798462c5bc5cf6d
Curtion <curtion@126.com>
2021-09-20 21:08:29 +0800

    添加key验证

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, Intents } = require("discord.js");
-const { token } = require("./config.json");
+const { token, pushkey } = require("./config.json");
 const axios = require("axios");
 const log4js = require("log4js");
 
@@ -27,7 +27,7 @@ client.on("messageCreate", (message) => {
     const author = message.authorId;
     if (author === "884623552584769546") {
       axios
-        .post(`https://steam.3gxk.net/emit.php`, message.embeds)
+        .post(`https://steam.3gxk.net/emit.php?key=${pushkey}`, message.embeds)
         .then((res) => {
           logger.info("推送消息成功：", res.data);
         })
